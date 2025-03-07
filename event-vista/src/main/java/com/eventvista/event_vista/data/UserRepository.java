2bf1d4a82588852f3bf2a3ec8ba43de667e47134
Maria Usina <maryusina@Marias-Air.attlocal.net>
2025-03-06 21:04:39 -0600

    Added UserRepo

---
+++ event-vista/src/main/java/com/eventvista/event_vista/data/UserRepository.java
@@ -0,0 +1,12 @@
+package com.eventvista.event_vista.data;
+
+import com.eventvista.event_vista.model.User;
+import jakarta.transaction.Transactional;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public interface UserRepository extends CrudRepository <User, Integer> {
+    User findByUsername(String username);
+}
