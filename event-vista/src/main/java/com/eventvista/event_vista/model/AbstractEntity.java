e73f025b302ab231af6d1ae2ea712b4d842e6726
Amanda <amandafaithdev@gmail.com>
2025-03-06 20:41:51 -0600

    initial setup push

---
+++ event-vista/src/main/java/com/eventvista/event_vista/model/AbstractEntity.java
@@ -0,0 +1,50 @@
+package com.eventvista.event_vista.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotBlank
+    @Size(max = 100)
+    private String name;
+
+    public int getId() {return id;}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
