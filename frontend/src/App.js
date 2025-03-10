601fba4fde0e130a78184d6e89110dabe355a75d
Maria Usina <maryusina@Marias-MacBook-Air.attlocal.net>
2025-03-10 15:28:38 -0500

    Initial setup of React app

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Start page for our event planner app
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
