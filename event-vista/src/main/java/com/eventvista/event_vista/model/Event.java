f9c7fe06d725326d0ce2d5704ae784735e3fe7aa
Amanda <amandafaithdev@gmail.com>
2025-03-17 15:49:07 -0500

    updated event class relationship with user, mapping, added LocalDate and LocalTime

--- event-vista/src/main/java/com/eventvista/event_vista/model/Event.java
+++ event-vista/src/main/java/com/eventvista/event_vista/model/Event.java
@@ -3,48 +3,59 @@ package com.eventvista.event_vista.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 @Entity
 public class Event extends AbstractEntity{
 
-    @OneToMany
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
     @NotBlank(message = "Field must have valid event name entered")
     @Size(min = 3, max = 100, message = "Field must be between 3 and 100 characters")
     private String name;
 
-    @NotBlank(message = "Field must have valid event date entered")
-    @DateTimeFormat
-    private String date;
+    @NotNull(message = "Field must have valid event date entered")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
 
-    @NotBlank(message = "Field must have valid event time entered")
-    @DateTimeFormat
-    private String time;
+    @NotNull(message = "Field must have valid event time entered")
+    @DateTimeFormat(pattern = "HH:mm")
+    private LocalTime time;
 
     @Size(max = 500, message = "Field must be less than 500 characters")
     private String notes;
 
     @ManyToOne
+    @JoinColumn(name = "venue_id")
     private Venue venue;
 
     @ManyToMany
+    @JoinTable(
+            name = "event_vendors",
+            joinColumns = @JoinColumn(name = "event_id"),
+            inverseJoinColumns = @JoinColumn(name = "vendor_id")
+    )
     private Set<Vendor> vendors = new HashSet<>();
 
     @ManyToOne
+    @JoinColumn(name = "client_id")
     private Client client;
 
     // Constructor
+
     public Event() {
     }
 
-    public Event(String name, String date, String time, String notes, Venue venue, Set<Vendor> vendors, Client client) {
+    public Event(String name, LocalDate date, LocalTime time, String notes, Venue venue, Set<Vendor> vendors, Client client) {
         this.name = name;
         this.date = date;
         this.time = time;
@@ -65,19 +76,19 @@ public class Event extends AbstractEntity{
         this.name = name;
     }
 
-    public String getDate() {
+    public LocalDate getDate() {
         return date;
     }
 
-    public void setDate(String date) {
+    public void setDate(LocalDate date) {
         this.date = date;
     }
 
-    public String getTime() {
+    public LocalTime getTime() {
         return time;
     }
 
-    public void setTime(String time) {
+    public void setTime(LocalTime time) {
         this.time = time;
     }
 
@@ -89,8 +100,20 @@ public class Event extends AbstractEntity{
         this.notes = notes;
     }
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     public Venue getVenue() {
         return venue;
+    }
+
+    public void setVenue(Venue venue) {
+        this.venue = venue;
     }
 
     public Set<Vendor> getVendors() {
