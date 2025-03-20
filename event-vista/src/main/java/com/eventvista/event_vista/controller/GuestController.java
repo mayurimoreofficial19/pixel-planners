c548e692a30a91bd59d517fb818b61d455186779
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2025-03-20 15:13:53 -0500

    GuestController has been updated

--- event-vista/src/main/java/com/eventvista/event_vista/controller/GuestController.java
+++ event-vista/src/main/java/com/eventvista/event_vista/controller/GuestController.java
@@ -1,5 +1,37 @@
 package com.eventvista.event_vista.controller;
 
+import com.eventvista.event_vista.model.Guest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import service.GuestService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/guests")
 public class GuestController {
 
+    @Autowired
+    private GuestService guestService;
+
+    @PostMapping("/add")
+    public Guest addGuest(@RequestBody Guest guest) {
+        return guestService.addGuest(guest);
+    }
+
+    @GetMapping("/all")
+    public List<Guest> getAllGuests() {
+        return guestService.getAllGuests();
+    }
+
+    @PutMapping("/update-rsvp")
+    public String updateRsvp(@RequestParam String email, @RequestParam boolean rsvp) {
+        return guestService.updateRsvp(email, rsvp) ? "RSVP updated successfully" : "Guest not found";
+    }
+
+    @DeleteMapping("/remove")
+    public String removeGuest(@RequestParam String email) {
+        return guestService.removeGuest(email) ? "Guest removed successfully" : "Guest not found";
+    }
 }
--- event-vista/src/main/java/com/eventvista/event_vista/data/GuestRepository.java
+++ event-vista/src/main/java/com/eventvista/event_vista/data/GuestRepository.java
@@ -3,6 +3,8 @@ package com.eventvista.event_vista.data;
 import com.eventvista.event_vista.model.Guest;
 import org.springframework.data.repository.CrudRepository;
 
-public interface GuestRepository extends CrudRepository<Guest, Integer> {
+import java.util.Optional;
 
+public interface GuestRepository extends CrudRepository<Guest, Integer> {
+    Optional<Guest> findByEmail(String email);
 }
