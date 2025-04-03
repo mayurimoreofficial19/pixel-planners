e746a21636d857775dcf1f08ca6a5b8b74824186
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2025-04-02 19:05:38 -0500

    Client-Setup updated

--- event-vista/src/main/java/com/eventvista/event_vista/service/ClientService.java
+++ event-vista/src/main/java/com/eventvista/event_vista/service/ClientService.java
@@ -14,22 +14,18 @@ public class ClientService {
         this.clientRepository = clientRepository;
     }
 
-    // Add a new client
     public Client addClient(Client client) {
         return clientRepository.save(client);
     }
 
-    // Get all clients
     public List<Client> getAllClients() {
         return clientRepository.findAll();
     }
 
-    // Search clients by name
     public List<Client> searchClientsByName(String name) {
         return clientRepository.findByNameContainingIgnoreCase(name);
     }
 
-    // Delete a client by ID
     public void deleteClient(Integer id) {
         clientRepository.deleteById(id);
     }
