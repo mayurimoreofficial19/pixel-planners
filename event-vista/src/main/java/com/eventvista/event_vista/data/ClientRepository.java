961dfad4c6e1b70349d43be6c4685ecba3d80a69
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2025-04-10 16:47:19 -0500

    ClientRepository updated

--- event-vista/src/main/java/com/eventvista/event_vista/data/ClientRepository.java
+++ event-vista/src/main/java/com/eventvista/event_vista/data/ClientRepository.java
@@ -1,6 +1,7 @@
 package com.eventvista.event_vista.data;
 
 import com.eventvista.event_vista.model.Client;
+import com.eventvista.event_vista.model.PhoneNumber;
 import com.eventvista.event_vista.model.User;
 import com.eventvista.event_vista.model.Venue;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,13 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<Client,Integer> {
-    List<Client> findByUser(User user);
+    List<Client> findAllByUser(User user);
+
+    Optional<Client> findByIdAndUser(Integer id, User user);
+
+    Optional<Client> findByNameAndUser(String name, User user);
+
+    Optional<Client> findByEmailAddressAndUser(String emailAddress, User user);
+
+    Optional<Client> findByPhoneNumberAndUser(PhoneNumber phoneNumber, User user);
 }
