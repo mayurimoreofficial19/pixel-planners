221816966d6ce67866fb24b35c80652875c549a0
Maria Usina <maryusina@Marias-MacBook-Air.attlocal.net>
2025-04-12 18:32:57 -0500

    Changed and added som styling to welcome page

--- frontend/src/pages/Welcome/Welcome.js
+++ frontend/src/pages/Welcome/Welcome.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import Header from '../Welcome/Header';
-import styles from '../Welcome/Welcome.module.css'; // Import the CSS file
+import styles from '../Welcome/Welcome.module.css';// Import the CSS file
+import { Link } from 'react-router-dom';
 
 const Welcome = () => {
   return (
     <div className={styles['welcome-container']}>
-    <Header />
     <div className={styles['welcome-content']}>
-      <h1>WELCOME</h1>
-
-
+      <h1 className={styles['welcome-title']}>WELCOME to EVENTVISTA</h1>
       <p>The portal will allow you to create and manage events</p>
+              <Link to="/login" className={styles['cta-button']}>
+                Get Started
+              </Link>
 
       </div>
 
